7523f80f8ce2ad0a4a36620323a5c945dc3a2ece
nemeszoltan <nemes_zoli@yahoo.com>
2019-08-29 10:32:52 +0300

    Added comments to declared functions

--- lib/TimeKeeper/TimeKeeper.h
+++ lib/TimeKeeper/TimeKeeper.h
@@ -3,10 +3,25 @@
 #ifndef _TIMEKEEPER_H_
 #define _TIMEKEEPER_H_
 
-void Set_Time();
-/* function to set the time in the RTC*/
 
+/**
+* @brief Function for setting time to RTC circuit
+*
+* Function for setting time to RTC circuit, expected input parameters 
+* year, month, day, hour, minute. Second not required as start value 
+* default vill be zero.
+*
+*/
+void Set_Time();
+
+/**
+* @brief Function for retrieving time from RTC circuit
+*
+* Function for retrieving time from RTC circuit, expected output parameters 
+* year, month, day, hour, minute, second. 
+*
+*/
 void Get_Time();
-/* function to get time from RTC*/
+
 
 #endif
